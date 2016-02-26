891b306e2268bb96ddf7b3204c9858fe363d8eff
Pierre Massat <pierre@depauloumassat.com>
2016-02-25 18:16:45 -0800

    Add sleep data

---
+++ sleep.go
@@ -0,0 +1,32 @@
+package garminconnect
+
+import (
+	"encoding/json"
+	"net/url"
+	"time"
+)
+
+type Sleep struct {
+	Date     string `json:"calendarDate"`
+	Duration int    `json:"sleepTimeSeconds"`
+}
+
+func (gc *GarminConnect) SleepByDate(date time.Time) Sleep {
+	params := url.Values{}
+	params.Set("date", date.Format("2006-01-02"))
+	params.Set("nonSleepBufferMinutes", "60")
+
+	response, err := gc.client.Get("https://connect.garmin.com/modern/proxy/wellness-service/wellness/dailySleep/user/" + gc.displayName + "?" + params.Encode())
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	var sleep Sleep
+
+	json.NewDecoder(response.Body).Decode(&sleep)
+
+	return sleep
+}
